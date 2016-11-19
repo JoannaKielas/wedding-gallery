901e47e05841023820d076bf1abc9fb74855f5fa
Joanna Kielas <jo.kielas@gmail.com>
2016-11-19 19:50:13 +0100

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,20 +3,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^photologue/', include('photologue_custom.urls')),
+    url(r'^photologue/', include('photologue_custom.urls',)),
     url(r'^photologue/', include('photologue.urls', namespace='photologue')),
     url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name= 'logout'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^$', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-#     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
