bb5f945d2dbea5c185618d402fa0e493bb8c567c
Joanna Kielas <jo.kielas@gmail.com>
2016-11-19 19:52:15 +0100

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,31 +1,20 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-
-from django.views.generic import TemplateView
-from braces.views import LoginRequiredMixin
+from django.contrib.auth import authenticate
+from django.contrib.auth.views import login, logout
+from django.shortcuts import HttpResponseRedirect, HttpResponse
 
 
+def my_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/')
+
+    else:
+        return HttpResponse('Logowanie niepoprawne')
 
 
-class LoginRequiredMixin(object):
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
-
-
-class SomeSecretView(LoginRequiredMixin, TemplateView):
-    template_name = "/photologue/gallerylist/"
-
-   
-
-    def get(self, request):
-        return self.render_to_response({})
-
-
-class PrivateView(PermissionsRequiredMixin, View):
-    required_permissions = (
-    'photologue.urls',
-    )
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Permission granted!')
+def my_logout(request):
+   logout(request)
+   return HttpResponseRedirect('/')
